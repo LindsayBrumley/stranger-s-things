9df7cd0ed78196f746a7163e595808b56b54b120
rylee <ryfrogquinn@gmail.com>
2023-04-28 09:51:40 -0500

    Rendered the posts to my profile

--- src/Components/MyProfile.jsx
+++ src/Components/MyProfile.jsx
@@ -4,29 +4,35 @@ import NewPostsForm from "./NewPostForm";
 import useAuth from "../useAuth";
 
 export function MyProfile() {
-  const { user } = useAuth();
+  const { token, user, setUser } = useAuth();
   const [posts, setPosts] = useState();
 
-  // useEffect(() => {
-  //   setPosts(user.posts);
-  // });
-  console.log(user);
+  useEffect(() => {
+    async function getMe() {
+      const response = await fetchMe(token);
+      setUser(response.data);
+    }
+    getMe();
+  }, []);
+  console.log("user from profile: ", user);
   return (
     <div>
-      <h3> My Profile</h3>
-      {/* <div>
-        {posts.map((post) => {
-          return (
-            <div key={post._id}>
-              <h3>{post.title}</h3>
-              <p>{post.description}</p>
-              <p>{post.price}</p>
-              <p>{post.location}</p>
-              <p>{post.willDeliver}</p>
-            </div>
-          );
-        })}
-      </div> */}
+      <h3>My Profile</h3>
+      <h3>Posts</h3>
+      <div>
+        {user.posts &&
+          user.posts.map((post) => {
+            return (
+              <div key={post._id}>
+                <h4>{post.title}</h4>
+                <p>{post.description}</p>
+                <p>{post.price}</p>
+                <p>{post.location}</p>
+                <p>{post.willDeliver}</p>
+              </div>
+            );
+          })}
+      </div>
       <NewPostsForm />
     </div>
   );
